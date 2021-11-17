3cc88dfcb181de8c6a3c510eecc69b23a67b0c8c
pan93412 <pan93412@gmail.com>
2021-11-18 07:03:23 +0800

    refactor(error): documented

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,15 @@
+//! UNM FFmpeg Server - 錯誤區塊
 use thiserror::Error;
 
 use crate::services::client::ClientServiceError;
 
+/// 這個 FFmpeg 伺服器發生的錯誤。
 #[derive(Error, Debug)]
 pub enum ServerError {
+    /// Client Service 發生的錯誤。
     #[error("services/client error: {0}")]
     ClientServiceError(ClientServiceError),
 }
 
+/// 這個 FFmpeg 伺服器的執行結果。
 pub type ServerResult<T> = Result<T, ServerError>;
